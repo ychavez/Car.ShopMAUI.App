3b1cfce623fcfef8e70897c88d5eac3ea6e00c4b
Yael Chavez <you400@gmail.com>
2022-10-26 08:40:02 -0500

    sqlite

---
+++ Context/CarShopConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Car.ShopMAUI.Context
+{
+    public static class CarShopConstants
+    {
+        public const string DatabaseFileName = "CarShop.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags
+            = SQLite.SQLiteOpenFlags.ReadWrite |
+            // crea la base de datos en caso de que no exista  
+            SQLite.SQLiteOpenFlags.Create |
+            // habilita el uso de mulihilos a la base de datos
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFileName);
+    }
+}
---
+++ Context/DataContext.cs
@@ -0,0 +1,50 @@
+﻿using Car.ShopMAUI.Models;
+using SQLite;
+
+namespace Car.ShopMAUI.Context
+{
+    public class DataContext
+    {
+        SQLiteAsyncConnection Database;
+
+        async Task Init() 
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(CarShopConstants.DatabasePath, CarShopConstants.Flags);
+            
+            var result = await Database.CreateTableAsync<Cars>();
+        
+        }
+
+        public async Task<List<Cars>> GetFavorites() 
+        {
+            await Init();
+            return await Database.Table<Cars>().ToListAsync();
+        }
+
+
+        public async Task<Cars> GetCarById(int id) 
+        {
+
+            await Init();
+            return await Database.Table<Cars>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task SetAsFavorite(Cars car) 
+        {
+
+            await Init();
+            var _car = await GetCarById(car.Id);
+
+            if (_car is not null)
+                return;
+
+             await Database.InsertAsync(car);
+
+        }
+
+
+    }
+}
